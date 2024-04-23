62c96c881ce939ec50fd24ceb573cb8835cb61ff
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-04-23 11:48:01 +0530

    Javascript Practice

--- function/fibbonaci.js
+++ function/fibbonaci.js
@@ -1,19 +1,11 @@
 //write a program to print the fibbonaci series.
-function fibonacci(num) {
-    if(num < 2) {
-        return num;
+function fibbonaci(number,n1,n2){
+    let nterms;
+    for(let i=1;i<=number;i++){
+        console.log(n1);
+        nterms = n1+n2;
+        n1 = n2;
+        n2 = nterms;
     }
-    else {
-        return fibonacci(num-1) + fibonacci(num - 2);
     }
-}
-let nTerms = 5;
-
-if(nTerms <=0) {
-    console.log('Enter a positive integer.');
-}
-else {
-    for(let i = 0; i < nTerms; i++) {
-        console.log(fibonacci(i));
-    }
-}
+    fibbonaci(5,0,1);
