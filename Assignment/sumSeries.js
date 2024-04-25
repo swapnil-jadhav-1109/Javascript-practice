f127a17d4c49f93df14e961673cc01b3a1a7872e
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-04-25 14:41:37 +0530

    Javascript Practice

--- Assignment/sumSeries.js
+++ Assignment/sumSeries.js
@@ -1,12 +1,22 @@
 //Write a program in js to calculate the sum of the series  9+99+999+9999 .... n.
-let num = 9;
-let series = 10;
-let sum = 0;
-for(let i = 1; i<= series; i++){
+// let num = 9;
+// let series = 10;
+// let sum = 0;
+// for(let i = 1; i<= series; i++){
+//     sum += num;
+//     console.log("the series of",num);
+//     num = num * 10 + 9;
+// }
+// console.log("sum of series is", sum);
+
+
+let seriess = (num,series)=>{
+let sum =0;
+for(i=0;i<=series;i++){
     sum += num;
-    console.log("the series of",num);
-    num = num * 10 + 9;
+    console.log(num);
+    num= num*10+9;
 }
-console.log("sum of series is", sum);
-
-
+return sum;
+}
+console.log(seriess(9,10));
